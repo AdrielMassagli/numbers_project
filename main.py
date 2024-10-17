3ffee706d6cb9f00422942be55591aea5ae1841b
AdrielMassagli <adriel.araujo@sumup.com>
2024-10-17 15:32:25 -0300

    Adding CAA WoW Calculation

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import snowflake.connector 
 import pandas as pd
 import os.path
+import datetime
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 
-ctx = snowflake.connector.connect(
-    user='ADRIEL.ARAUJO@SUMUP.COM',
-    password='25Abc09*',
-    host= 'ui58164.eu-west-1.snowflakecomputing.com',
-    authenticator= 'externalbrowser',
-    account='ui58164',
-    Role= 'SNOWFLAKE_SALES_INTELIGENCE_BR_ANALYST',
-    Warehouse= "SALES_INTELLIGENCE_BR_DBT_WH_PROD", 
-    Database= 'SALES_INTELLIGENCE_BR_PROD')
-cs = ctx.cursor()
-sql_database = 'Use SALES_INTELLIGENCE_BR_PROD'
-cs.execute(sql_database)
-cs.execute('ALTER SESSION SET WEEK_START = 7')
+
+# ctx = snowflake.connector.connect(
+#     user='ADRIEL.ARAUJO@SUMUP.COM',
+#     password='25Abc09*',
+#     host= 'ui58164.eu-west-1.snowflakecomputing.com',
+#     authenticator= 'externalbrowser',
+#     account='ui58164',
+#     Role= 'SNOWFLAKE_SALES_INTELIGENCE_BR_ANALYST',
+#     Warehouse= "SALES_INTELLIGENCE_BR_DBT_WH_PROD", 
+#     Database= 'SALES_INTELLIGENCE_BR_PROD')
+# cs = ctx.cursor()
+# sql_database = 'Use SALES_INTELLIGENCE_BR_PROD'
+# cs.execute(sql_database)
+# cs.execute('ALTER SESSION SET WEEK_START = 7')
 
 # Escopo da API.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
@@ -60,52 +62,53 @@ def clear_all():
     ).execute()
 #clear_all()
 
-def input_tpv():
-    with open(".\querys\query_tpv.sql", "r") as file:
-       query_tpv = file.read()
-    query_tpv = query_tpv.replace(":date:", date)
-    cs.execute(query_tpv)
-    data = cs.fetch_pandas_all()
+# def input_tpv():
+#     with open(".\querys\query_tpv.sql", "r") as file:
+#        query_tpv = file.read()
+#     query_tpv = query_tpv.replace(":date:", date)
+#     cs.execute(query_tpv)
+#     data = cs.fetch_pandas_all()
 
-    cohort_list = data['COHORT'].tolist()
-    tx_week_list = data['TX_WEEK'].tolist()
-    channel_list = data['ACQUISITION_CHANNEL'].tolist()
-    subchannel_list = data['ACQUISITION_SUBCHANNEL'].tolist()
-    tpv_list = [str(value) for value in data['TPV'].tolist()]
+#     cohort_list = data['COHORT'].tolist()
+#     tx_week_list = data['TX_WEEK'].tolist()
+#     channel_list = data['ACQUISITION_CHANNEL'].tolist()
+#     subchannel_list = data['ACQUISITION_SUBCHANNEL'].tolist()
+#     tpv_list = [str(value) for value in data['TPV'].tolist()]
 
-    input_datas = [cohort_list, tx_week_list, channel_list, subchannel_list, tpv_list]
-    sheet.values().update(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID, 
-        range='db_tpv_add!A2', 
-        valueInputOption="USER_ENTERED",
-        body={'majorDimension': 'COLUMNS','values': input_datas}
-    ).execute()
-# input_tpv()
+#     input_datas = [cohort_list, tx_week_list, channel_list, subchannel_list, tpv_list]
+#     sheet.values().update(
+#         spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+#         range='db_tpv_add!A2', 
+#         valueInputOption="USER_ENTERED",
+#         body={'majorDimension': 'COLUMNS','values': input_datas}
+#     ).execute()
+# # input_tpv()
 
-def input_cro():
-   with open(".\querys\query_cro.sql", "r") as file:
-      query_cro = file.read()
-   query_cro = query_cro.replace(":date:", date_2)
-   print(query_cro)
-   cs.execute(query_cro)
-   data = cs.fetch_pandas_all()
+# def input_cro():
+#    with open(".\querys\query_cro.sql", "r") as file:
+#       query_cro = file.read()
+#    query_cro = query_cro.replace(":date:", date_2)
+#    print(query_cro)
+#    cs.execute(query_cro)
+#    data = cs.fetch_pandas_all()
 
-   cohort_list = data['COHORT'].tolist()
-   channel_list = data['ACQUISITION_CHANNEL'].tolist()
-   subchannel_list = data['ACQUISITION_SUBCHANNEL'].tolist()
-   merchant_list = data['MERCHANT_CODE'].tolist()
+#    cohort_list = data['COHORT'].tolist()
+#    channel_list = data['ACQUISITION_CHANNEL'].tolist()
+#    subchannel_list = data['ACQUISITION_SUBCHANNEL'].tolist()
+#    merchant_list = data['MERCHANT_CODE'].tolist()
 
-   input_datas = [cohort_list, channel_list, subchannel_list, merchant_list]
-   sheet.values().update(
-        spreadsheetId=SAMPLE_SPREADSHEET_ID, 
-        range='db_new_merchants!A2', 
-        valueInputOption="USER_ENTERED",
-        body={'majorDimension': 'COLUMNS','values': input_datas}
-    ).execute()
-   print(input_datas)
-# input_cro()
+#    input_datas = [cohort_list, channel_list, subchannel_list, merchant_list]
+#    sheet.values().update(
+#         spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+#         range='db_new_merchants!A2', 
+#         valueInputOption="USER_ENTERED",
+#         body={'majorDimension': 'COLUMNS','values': input_datas}
+#     ).execute()
+#    print(input_datas)
+# # input_cro()
 
 def get_v2():
+   #puxando os dados de CAA da planilha
    result = (
         sheet.values()
         .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, 
@@ -113,25 +116,75 @@ def get_v2():
              majorDimension='ROWS')
         .execute()
     )
-   #print(result['values'][0][104])
-   #print(len(result['values'][0]))
+   #definindo o dia da semana de acordo com a data
+   data = datetime.datetime.strptime(date, '%Y-%m-%d')
+   num_week = data.strftime('%U')
+   num_week = int(num_week)+1
+   search = "W:week:/2024" #padrão de pesquisa
+   search = search.replace(":week:", str(num_week))
+   print(search)
 
+   #looping que passa em todos os registros da tabela e encontra o valor de cada canal na semana
    i = 0
-   x = 0
-   pesquisa = "W52/2024"
-
    while i < len(result['values'][0]):
-      if result['values'][0][i] == pesquisa:
-         while x < len(result['values'][2]):
-            if result['values'][2][x] == result['values'][0][i]:
-               result['values'][2][x]
+      if result['values'][0][i] == search:
+         #CAA
+         actual_week_caa = float(result['values'][1][i])
+         last_week_caa = float(result['values'][1][i-1])
+         percent_caa = ((actual_week_caa-last_week_caa)/last_week_caa)*100
+
+         #Executives
+         actual_week_executives = float(result['values'][2][i])
+         last_week_executives = float(result['values'][2][i-1])
+         percent_executives = ((actual_week_executives-last_week_executives)/last_week_executives)*100
+
+         #OM
+         actual_week_om = float(result['values'][3][i])
+         last_week_om = float(result['values'][3][i-1])
+         percent_om = ((actual_week_om-last_week_om)/last_week_om)*100
+
+         #Consultants
+         actual_week_consultants = float(result['values'][4][i])
+         last_week_consultants = float(result['values'][4][i-1])
+         percent_consultants = ((actual_week_consultants-last_week_consultants)/last_week_consultants)*100
+
+         #RaF
+         actual_week_raf = float(result['values'][5][i])
+         last_week_raf = float(result['values'][5][i-1])
+         percent_raf = ((actual_week_raf-last_week_raf)/last_week_raf)*100
+
+         #Integradores
+         actual_week_integra = float(result['values'][6][i])
+         last_week_integra = float(result['values'][6][i-1])
+         percent_integra = ((actual_week_integra-last_week_integra)/last_week_integra)*100
+
+         #Second hand
+         actual_week_second = float(result['values'][7][i])
+         last_week_second = float(result['values'][7][i-1])
+         percent_second = ((actual_week_second-last_week_second)/last_week_second)*100
+
+         input_result = [[round(percent_executives, 1)], [round(percent_om,1)], [round(percent_consultants,1)], [round(percent_raf,1)], [round(percent_second,1)], [round(percent_integra,1)], [0], [round(percent_caa,1)] ]
+
+         print(input_result)
          print("Deu certo")
          i += 1
-      else: 
-         print("não deu certo")
+      else:
          i += 1 
-    
 
+   sheet.values().update(
+         spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+         range='Análise Overall!B3', 
+         valueInputOption="USER_ENTERED",
+         body={'majorDimension': 'ROWS','values': input_result}
+    ).execute()
+   
+   sheet.values().update(
+         spreadsheetId=SAMPLE_SPREADSHEET_ID, 
+         range='Análise Overall!A2', 
+         valueInputOption="USER_ENTERED",
+         body={'majorDimension': 'ROWS','values': [[search]]}
+    ).execute()
+get_v2()
 
 
 
